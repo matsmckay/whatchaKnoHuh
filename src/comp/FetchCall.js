fb4d348d829bcc2d0eebedb7a12be3979f6f84c8
damanM <daman.m@outlook.com>
2022-12-08 22:36:05 -0500

    made api call got homo word back and def of word and display on page :sunglasses: :fire: :tophat:

--- src/comp/FetchCall.js
+++ src/comp/FetchCall.js
@@ -9,23 +9,39 @@ const FetchCall = () => {
     useEffect(() => {
 
         axios({
-            url: "api.datamuse.com/words",
-            method: "GET",
-            dataResponse: "json",
-            params : {
-              rel_hom: "course" 
-            }
-        }).then((res) => {
-            console.log(res);
-            setHomoWords(res)
-        })
+          url: `https://api.datamuse.com/words`,
+          method: "GET",
+          dataResponse: "json",
+          params: {
+            rel_hom: "course",
+            md: "d"
+          },
+        }).then((response) => {
+          const similarSound = response.data; 
+          setHomoWords(similarSound);
 
+          
+        });
+        
     }
     ,[])
-
+    
+    console.log(homoWords);
     return (
         <div>
             <h2>Issa Test Yoo Reelaxxx</h2>
+
+        {
+            homoWords.map((word) => {
+                return (
+                    <div>
+                        <h3 key= {word.score}> Word: {word.word} </h3>
+                        <p> Def: { word.defs} </p>
+                    </div>
+                )
+            })
+        }
+           
 
         </div>
     )
