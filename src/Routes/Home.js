1c7460187bc15e26f62f941931188ed5c35937c0
MattMcKay <matthewmckay@gmail.com>
2022-12-12 16:56:18 -0500

    merging router-branch with devBranch

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,14 @@
 import { Link, Outlet } from 'react-router-dom'
+import FetchCall from '../comp/FetchCall';
+import Leaderboard from './Leaderboard';
 
 const Home = () => {
     return (
         <div>
             <h2>Welcome Home!</h2>
             <div className="divBtn">
+                <FetchCall />
+                <Leaderboard />
                 <button>
                     <Link to="/playNow">Play Now!</Link>
                 </button>
