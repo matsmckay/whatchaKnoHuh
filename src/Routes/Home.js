a9dca03a9f609109b6aa64045d39bdb5a373123a
damanM <daman.m@outlook.com>
2022-12-19 17:35:58 -0500

    added wrapper

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
  
  const Home = () => {
     return (
-        <div>
+        <div className='wrapper'>
             <h2>Home</h2>
             <section className='howToPlay'>
                 <div>            
