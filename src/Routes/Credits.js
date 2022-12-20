549096ccc58a2e88d7157330885f909c0e2550a2
MattMcKay <matthewmckay@gmail.com>
2022-12-19 20:48:07 -0500

    minor styling to credits page

--- src/Routes/Credits.js
+++ src/Routes/Credits.js
@@ -4,23 +4,25 @@ import { Link, Outlet } from 'react-router-dom';
 const Credits = () => {
     return (
         <div className="creditsDiv">
-            <h2>MEET THE TEAM</h2>
-            {/* Outlet is where on the page you want stuff to render */}
-            <ul className="creditsTeamUl">
-                <li className="creditsTeamLi">
-                    <Link to="/credits/radojko" className="team">Radojko Radulovic</Link>
-                </li>
-                <li className="creditsTeamLi">
-                    <Link to="/credits/daman" className="team">Daman Matharoo</Link>
-                </li>
-                <li className="creditsTeamLi">
-                    <Link to="/credits/tyler" className="team">Tyler George</Link>
-                </li>
-                <li className="creditsTeamLi">
-                    <Link to="/credits/matt" className="team">Matt McKay</Link>
-                </li>
-            </ul>
-            <Outlet />
+            <div className="wrapper">
+                <h2>MEET THE TEAM</h2>
+                {/* Outlet is where on the page you want stuff to render */}
+                <ul className="creditsTeamUl">
+                    <li className="creditsTeamLi">
+                        <Link to="/credits/radojko" className="team">Radojko Radulovic</Link>
+                    </li>
+                    <li className="creditsTeamLi">
+                        <Link to="/credits/daman" className="team">Daman Matharoo</Link>
+                    </li>
+                    <li className="creditsTeamLi">
+                        <Link to="/credits/tyler" className="team">Tyler George</Link>
+                    </li>
+                    <li className="creditsTeamLi">
+                        <Link to="/credits/matt" className="team">Matthew McKay</Link>
+                    </li>
+                </ul>
+                <Outlet />
+            </div>
         </div>
     )
 }
