cf0fdbca531a94dc1c389fc2777f5aaff71444a8
MattMcKay <matthewmckay@gmail.com>
2022-12-20 09:35:10 -0500

    styling done to credits section almost complete

--- src/Routes/Credits.js
+++ src/Routes/Credits.js
@@ -10,34 +10,34 @@ const Credits = () => {
                 <ul className="creditsTeamUl">
                     <li className="creditsTeamLi">
                         <img className="creditImg" src="https://ca.slack-edge.com/T02E8L5NKFV-U044ASBKNFM-bc3823597214-512" alt="" />
-                        <Link to="/credits/radojko" className="team">Radojko Radulovic</Link>
+                        <h3 className="team">Radojko Radulovic</h3>
                         <p className="creditBio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste provident, inventore excepturi nesciunt neque sed temporibus error explicabo dolorem, quo ducimus sit! Porro atque veniam est animi tempore quaerat ut.</p>
-                        <p><a href="https://www.radojkoradulovic.com">radojkoradulovic.com</a></p>
-                        <p><a className="email" href="mailto:radulovicradojko@gmail.com">Contact</a></p>
+                        <p className="creditsHomepage"><a href="https://www.radojkoradulovic.com">radojkoradulovic.com</a></p>
+                        <p><a className="creditsEmail" href="mailto:radulovicradojko@gmail.com">Contact</a></p>
                     </li>
                     <li className="creditsTeamLi">
                         <img className="creditImg" src="https://ca.slack-edge.com/T02E8L5NKFV-U044PJ6CK41-98d008615619-512" alt="" />
-                        <Link to="/credits/daman" className="team">Daman Matharoo</Link>
+                        <h3 className="team">Daman Matharoo</h3>
                         <p className="creditBio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste provident, inventore excepturi nesciunt neque sed temporibus error explicabo dolorem, quo ducimus sit! Porro atque veniam est animi tempore quaerat ut.</p>
-                        <p><a href="https://www.damanmatharoo.com">damanmatharoo.com</a></p>
-                        <p><a className="email" href="mailto:damanmatharoo4@gmail.com">Contact</a></p>
+                        <p className="creditsHomepage"><a href="https://www.damanmatharoo.com">damanmatharoo.com</a></p>
+                        <p><a className="creditsEmail" href="mailto:damanmatharoo4@gmail.com">Contact</a></p>
                     </li>
                     <li className="creditsTeamLi">
                         <img className="creditImg" src="https://ca.slack-edge.com/T02E8L5NKFV-U044PJ67L2D-9fd4f1335035-512" alt="" />
-                        <Link to="/credits/tyler" className="team">Tyler George</Link>
+                        <h3 className="team">Tyler George</h3>
                         <p className="creditBio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste provident, inventore excepturi nesciunt neque sed temporibus error explicabo dolorem, quo ducimus sit! Porro atque veniam est animi tempore quaerat ut.</p>
-                        <p><a href="https://www.tylergeorge.com">tylergeorge.com</a></p>
-                        <p><a className="email" href="mailto:tylerGeorge@email.com">Contact</a></p>
+                        <p className="creditsHomepage"><a href="https://www.tylergeorge.com">tylergeorge.com</a></p>
+                        <p><a className="creditsEmail" href="mailto:tylerGeorge@email.com">Contact</a></p>
                     </li>
                     <li className="creditsTeamLi">
                         <img className="creditImg" src="https://ca.slack-edge.com/T02E8L5NKFV-U0444AVFR9U-7196a93dd413-512" alt="" />
-                        <Link to="/credits/matt" className="team">Matthew McKay</Link>
-                        <p className="creditBio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ab nobis eaque officiis at culpa dolor ipsam sint! Nulla eligendi earum mollitia, labore aliquid molestias? Nostrum, magni neque! Distinctio, aut?</p>
-                        <p><a href="https://www.matthewmckay.com">matthewmckay.com</a></p>
-                        <p><a className="email" href="mailto:matthewmckay@gmail.com">Contact</a></p>
+                        <h3 className="team">Matthew McKay</h3>
+                        <p className="creditBio">Front end developer based in Toronto specializing in HTML5, CSS, JAVASCRIPT and REACT. After almost 15 years running my own construction company, I’m going  in another direction, exchanging my tool belt for a keyboard to build websites instead of homes.</p>
+                        <p className="creditsHomepage"><a href="https://www.matthewmckay.com">matthewmckay.com</a></p>
+                        <p><a className="creditsEmail" href="mailto:matthewmckay@gmail.com">Contact</a></p>
                     </li>
+                    {/* <Outlet /> */}
                 </ul>
-                <Outlet />
             </div>
         </div>
     )
