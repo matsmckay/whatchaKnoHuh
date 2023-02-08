865d94c7c6872e814f47f221c0e57bd4ca3b0f7e
damanM <daman.m@outlook.com>
2023-02-08 16:44:55 -0500

    added confetti animation once player completes game and added hover animation on score submit button :tophat: :fire: :sparkles:

--- src/Routes/LeaderBoardFormPage.js
+++ src/Routes/LeaderBoardFormPage.js
@@ -1,32 +1,50 @@
 import LeaderBoardForm from '../comp/LeaderBoardForm'
 import { useLocation } from "react-router-dom"
 import { useState, useEffect} from 'react'
+import ReactConfetti from 'react-confetti'
 
 const LeaderBoardFormPage = ({userScore}) => {
 
-  const [isVisible, setIsVisiable] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
+  const [windowSize, setWindowSize] = useState({width: window.innerWidth, height: window.innerHeight})
+
   const location = useLocation()
 
+  const detectSize = () => {
+    setWindowSize({width: window.innerWidth, height: window.innerHeight});
+  }
+
   useEffect(() => {
-    setIsVisiable(true);
-  }, []);
+      setIsVisible(true);
+      window.addEventListener('resize', detectSize);
+      return() => {
+        window.removeEventListener('resize', detectSize);
+      }
+  }, [windowSize]);
 
   return(
       <div>
-      <div className={`fadeIn ${isVisible ? 'visiable' : ''}`}>
-        <div className='congrats'>Congratulations!</div> 
-      </div>
-        <section className="FormPage">
-          <div className="wrapper">
-            <div className="formStyling">
-              <h2>Your Score was:</h2>
-              <p>{location.state.userScore}</p>
-              <LeaderBoardForm
-              userScore={userScore}
-              />
-            </div>
+          <ReactConfetti 
+          tweenDuration={100} 
+          width={windowSize.width}
+          height={windowSize.height}
+          />
+          <div className={`fadeIn ${isVisible ? 'visiable' : ''} `}>
+              <div className='congrats'>Congratulations!</  div> 
           </div>
-        </section>
+
+          <section className="FormPage">
+            <div className="wrapper">
+              <div className="formStyling">
+                <h2>Your Score was:</h2>
+                <p>{location.state.userScore}</p>
+                <LeaderBoardForm
+                userScore={userScore}
+                />
+              </div>
+            </div>
+          </section>
+
       </div>
   )
 }
